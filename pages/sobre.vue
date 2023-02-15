00dff8fd798059344aeec2a83f8ab939c5fe112e
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2023-02-15 12:10:46 -0500

    Pagina sobre conectada na api

--- pages/sobre.vue
+++ pages/sobre.vue
@@ -3,12 +3,11 @@
         <!-- Page Header Start -->
     <div class="container-fluid page-header py-5 mb-5">
         <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown">About Us</h1>
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Sobre Nós</h1>
             <nav aria-label="breadcrumb animated slideInDown">
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
-                    <li class="breadcrumb-item text-white active" aria-current="page">About</li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">Sobre</li>
                 </ol>
             </nav>
         </div>
@@ -25,9 +24,9 @@
                         <div class="btn-lg-square bg-primary rounded-circle me-3">
                             <i class="fa fa-users text-white"></i>
                         </div>
-                        <h1 class="mb-0" data-toggle="counter-up">3453</h1>
+                        <h1 class="mb-0" data-toggle="counter-up">{{ home.data.attributes.Numeros.Clientes }}</h1>
                     </div>
-                    <h5 class="mb-3">Happy Customers</h5>
+                    <h5 class="mb-3">Clientes Felizes</h5>
                     <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
                 </div>
                 <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
@@ -35,9 +34,9 @@
                         <div class="btn-lg-square bg-primary rounded-circle me-3">
                             <i class="fa fa-check text-white"></i>
                         </div>
-                        <h1 class="mb-0" data-toggle="counter-up">4234</h1>
+                        <h1 class="mb-0" data-toggle="counter-up">{{ home.data.attributes.Numeros.Projetos }}</h1>
                     </div>
-                    <h5 class="mb-3">Project Done</h5>
+                    <h5 class="mb-3">Projetos Finalizados</h5>
                     <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
                 </div>
                 <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.5s">
@@ -45,9 +44,9 @@
                         <div class="btn-lg-square bg-primary rounded-circle me-3">
                             <i class="fa fa-award text-white"></i>
                         </div>
-                        <h1 class="mb-0" data-toggle="counter-up">3123</h1>
+                        <h1 class="mb-0" data-toggle="counter-up">{{ home.data.attributes.Numeros.Premios }}</h1>
                     </div>
-                    <h5 class="mb-3">Awards Win</h5>
+                    <h5 class="mb-3">Premios</h5>
                     <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
                 </div>
                 <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
@@ -55,15 +54,16 @@
                         <div class="btn-lg-square bg-primary rounded-circle me-3">
                             <i class="fa fa-users-cog text-white"></i>
                         </div>
-                        <h1 class="mb-0" data-toggle="counter-up">1831</h1>
+                        <h1 class="mb-0" data-toggle="counter-up">{{ home.data.attributes.Numeros.Funcionarios }}</h1>
                     </div>
-                    <h5 class="mb-3">Expert Workers</h5>
+                    <h5 class="mb-3">Funcionários</h5>
                     <span>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit</span>
                 </div>
             </div>
         </div>
     </div>
     <!-- Feature Start -->
+
     
 
     <!-- About Start -->
@@ -77,13 +77,14 @@
                 </div>
                 <div class="col-lg-6 about-text py-5 wow fadeIn" data-wow-delay="0.5s">
                     <div class="p-lg-5 pe-lg-0">
-                        <h6 class="text-primary">About Us</h6>
-                        <h1 class="mb-4">25+ Years Experience In Solar & Renewable Energy Industry</h1>
-                        <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo erat amet</p>
-                        <p><i class="fa fa-check-circle text-primary me-3"></i>Diam dolor diam ipsum</p>
-                        <p><i class="fa fa-check-circle text-primary me-3"></i>Aliqu diam amet diam et eos</p>
-                        <p><i class="fa fa-check-circle text-primary me-3"></i>Tempor erat elitr rebum at clita</p>
-                        <a href="" class="btn btn-primary rounded-pill py-3 px-5 mt-3">Explore More</a>
+                        <h6 class="text-primary">Sobre Nós</h6>
+                        <h1 class="mb-4">{{ sobre.data.attributes.Titulo }}</h1>
+                        <p>{{ sobre.data.attributes.Descricao }}</p>
+                        <p v-for="atributo in sobre.data.attributes.Atributos" :key="atributo.id">
+                            <i class="fa fa-check-circle text-primary me-3"></i>
+                            {{ atributo.Descricao }}
+                        </p>
+                        <a href="/servicos" class="btn btn-primary rounded-pill py-3 px-5 mt-3">Saiba Mais</a>
                     </div>
                 </div>
             </div>
@@ -96,61 +97,35 @@
     <div class="container-xxl py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <h6 class="text-primary">Team Member</h6>
-                <h1 class="mb-4">Experienced Team Members</h1>
+                <h6 class="text-primary">Membros Do Time</h6>
+                <h1 class="mb-4">Valiosos Membros Da Equipe</h1>
             </div>
             <div class="row g-4">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div v-for="membro in sobre.data.attributes.Diretoria" :key="membro.id" class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="team-item rounded overflow-hidden">
                         <div class="d-flex">
-                            <img class="img-fluid w-75" src="/static/img/team-1.jpg" alt="">
+                            <img class="img-fluid w-75" :src="membro.Foto.data.attributes.url" alt="">
                             <div class="team-social w-25">
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-instagram"></i></a>
+                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" :href="membro.facebook"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" :href="membro.twitter"><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" :href="membro.instagram"><i class="fab fa-instagram"></i></a>
                             </div>
                         </div>
                         <div class="p-4">
-                            <h5>Full Name</h5>
-                            <span>Designation</span>
+                            <h5>{{ membro.Nome }}</h5>
+                            <span>{{ membro.Cargo }}</span>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="team-item rounded overflow-hidden">
-                        <div class="d-flex">
-                            <img class="img-fluid w-75" src="/static/img/team-2.jpg" alt="">
-                            <div class="team-social w-25">
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                        <div class="p-4">
-                            <h5>Full Name</h5>
-                            <span>Designation</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="team-item rounded overflow-hidden">
-                        <div class="d-flex">
-                            <img class="img-fluid w-75" src="/static/img/team-3.jpg" alt="">
-                            <div class="team-social w-25">
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-lg-square btn-outline-primary rounded-circle mt-3" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                        <div class="p-4">
-                            <h5>Full Name</h5>
-                            <span>Designation</span>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
     <!-- Team End -->
     </main>
 </template>
+
+<script setup>
+const { data: home } = await useFetch("https://strapi-production-b8ce.up.railway.app/api/home?populate=deep")
+const { data: sobre } = await useFetch("https://strapi-production-b8ce.up.railway.app/api/sobre?populate=deep")
+</script>
